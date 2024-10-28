00d3d87838ccf8f0720d51b65ce389e9008b1899
Andrzejczak2021 <95053614+Felipi2021@users.noreply.github.com>
2024-10-28 18:48:13 +0100

    Update zad28102024.sql

--- zad28102024/zad28102024.sql
+++ zad28102024/zad28102024.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2024 at 06:28 PM
+-- Generation Time: Oct 28, 2024 at 06:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
