c68c81b048b1a70cd2dfeb2ba0c2378a7ca3997e
Andrzejczak2021 <95053614+Felipi2021@users.noreply.github.com>
2025-04-14 01:14:41 +0200

    Delete sprawdzian_Filip_Andrzejczak_4D directory

--- sprawdzian_Filip_Andrzejczak_4D/models/Teacher.py
+++
@@ -1,8 +0,0 @@
-class Teacher:
-    def __init__(self, _id: int, name: str, surname: str) -> None:
-        self._id = _id
-        self.name = name
-        self.surname = surname
-
-    def __str__(self) -> str:
-        return f"{self.name} {self.surname}"
--- sprawdzian_Filip_Andrzejczak_4D/models/__init__.py
+++
@@ -1,2 +0,0 @@
-__copyright__ = "Zespół Szkół Komunikacji"
-__author__ = "Filip Andrzejczak 4D"
